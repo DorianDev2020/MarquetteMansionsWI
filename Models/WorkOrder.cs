845fbd733682fa2150a875901ee54337b177fd1f
DorianHaley <59707012+DorianHaley@users.noreply.github.com>
2020-03-27 11:10:55 -0500

    Created WorkOrder Model

--- Models/WorkOrder.cs
+++ Models/WorkOrder.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +9,11 @@ namespace Marquette_Mansions.Models
 {
     public class WorkOrder
     {
-
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("Tenant")]
+        public int TenantId { get; set; }
+        [Display(Name = "Work Order Request")]
+        public string MaintenanceOrder { get; set; }
     }
 }
