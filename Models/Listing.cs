90b18fd5a4808b4fd0c14b8946c8d38159352981
DorianHaley <59707012+DorianHaley@users.noreply.github.com>
2020-03-27 11:15:35 -0500

    created the Listing Model

--- Models/Listing.cs
+++ Models/Listing.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +9,22 @@ namespace Marquette_Mansions.Models
 {
     public class Listing
     {
-
+        [Key]
+        public int ListingID { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+        public int SquareFeet { get; set; }
+        public int Beds { get; set; }
+        public int Baths { get; set; }
+        public bool Available { get; set; }
+        [ForeignKey("Manager")]
+        public int? LandLordId { get; set; }
+        public Manager Manager { get; set; }
+        public int? AdminID { get; set; }
+        [ForeignKey("Tennant")]
+        public int? TennantID { get; set; }
+        public Tennant Tennant { get; set; }
     }
 }
