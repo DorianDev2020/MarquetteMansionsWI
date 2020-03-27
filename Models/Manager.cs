c5c619c9d9c12c4b2b92cd5c68736dac5beac314
DorianHaley <59707012+DorianHaley@users.noreply.github.com>
2020-03-27 11:13:13 -0500

    created the Manager Model

--- Models/Manager.cs
+++ Models/Manager.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +10,16 @@ namespace Marquette_Mansions.Models
 {
     public class Manager
     {
+        [Key]
+        public int AdminID { get; set; }
+        //this is for the Roles
+        [ForeignKey("IdentityUser")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
+
+        public string EmailAddress { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int PhoneNumber { get; set; }
     }
 }
